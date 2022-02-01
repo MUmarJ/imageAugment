6ce2a26c96f98a4d998bfeae13e6e6c12fc49d59
MUmarJ <umarjh96@gmail.com>
2022-01-31 19:10:16 -0600

    UPDATE: Add save functionality to randomPlaceTool

--- randomPlaceTool.py
+++ randomPlaceTool.py
@@ -1,49 +1,49 @@
+import os
 import cv2
 import matplotlib.pyplot as plt
-import numpy as np
 import re
-import os
 import time
-import glob, random
+import random
 import math
-
 from utils import *
-from PIL import Image
-import tkinter as tk
-
-from tkinter import filedialog
 from skimage.io import imread_collection
-
-root = tk.Tk()
-root.withdraw()
-
-import os, random
 
 col_dir = "./testTools/*.png"
 
-# creating a collection with the available images
+# Specify number of images to generate
+generationCount = 5
+
+# Creating a collection with the available images
 col = imread_collection(col_dir)
 trayPath = "emptyTrays/RelineCore1LevelB_crop.jpeg"
+trayName = re.search(r"[\/\\](\w+)", trayPath)[1]
 tray = cv2.imread(trayPath)
+
+outputFolder = ".\\testOutput\\"
+fileName = None
+timestr = time.strftime("%Y%m%d-%H%M%S")
 # tool = cv2.rotate(tool, cv2.cv2.ROTATE_90_CLOCKWISE)
 
-generationCount = 5
+
 results = []
 for n in range(generationCount):
     trayCopy = tray.copy()
+
+    # Each generated image name in sequence will have an index differentiating it
+    fileName = f"{trayName}_{n}_{timestr}"
     for i in range(random.randint(0, len(col) - 1)):
         randomIndex = random.randint(0, len(col) - 1)
-        # print(randomIndex)
         tool = col[randomIndex].copy()
         toolCopy = tool.copy()
         tool = image_resize(toolCopy, scale=0.75, reference=tool)
         tool = tool[:, :, :4]
+
         alpha_mask = (tool[:, :, 3].copy()) / 255.0
         img_overlay = tool[:, :, :3].copy()
 
         trayHeight, trayWidth, _ = tray.shape
-        # print(trayHeight, trayWidth)
 
+        # Bounds for placing tools within
         lowerBound = 0.2
         upperBound = 0.8
 
@@ -56,7 +56,22 @@ for n in range(generationCount):
                 math.floor(lowerBound * trayHeight), math.ceil(upperBound * trayHeight)
             ),
         )
+
+        # Overlay tool on tray on x and y coordinates
         overlay_image_alpha(trayCopy, img_overlay, x, y, alpha_mask)
+
+        # Set file saving path
+        path = os.path.join(outputFolder + fileName)
+
+        # Save generated image
+        cv2.imwrite(path + ".jpg", trayCopy)
+
+        # Find tool name from tool file path and save its bounding box data
+        toolName = re.search(r"\\(\w+)[^\.]*", col.files[randomIndex])[1]
+        toolWidth, toolHeight = alpha_mask.shape
+        with open((path + ".txt"), "a") as f:
+            f.write(f"{toolName} {x} {y} {toolWidth} {toolHeight}\n")
+
     results.append(trayCopy)
 
 # cv2.namedWindow("image", cv2.WINDOW_NORMAL)
@@ -66,18 +81,18 @@ for n in range(generationCount):
 
 
 # Display resultant images code
-w = 10
-h = 10
-fig = plt.figure(figsize=(5, 1))
-columns = 5
-rows = 1
+# w = 10
+# h = 10
+# fig = plt.figure(figsize=(5, 1))
+# columns = 5
+# rows = 1
 
-imgIndex = 0
-print(len(results))
-for i in range(1, columns * rows + 1):
-    print(imgIndex)
-    img = results[imgIndex]
-    imgIndex += 1
-    fig.add_subplot(rows, columns, i)
-    plt.imshow(img)
-plt.show()
+# imgIndex = 0
+# print(len(results))
+# for i in range(1, columns * rows + 1):
+#     print(imgIndex)
+#     img = results[imgIndex]
+#     imgIndex += 1
+#     fig.add_subplot(rows, columns, i)
+#     plt.imshow(img)
+# plt.show()
